ced5e97bf38f0fe211fda22c4494685cd23c1915
Sergio1903-hl <a24310371@live.ceti.mx>
2025-09-06 01:00:45 +0000

    Practica 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
+#include <Foco.hpp>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "hola mundo" << endl;
+    Foco foquito;
+    foquito.Encender();
+    foquito.Apagar();
     return 0;
 }
